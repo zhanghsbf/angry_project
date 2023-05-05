9192a88ea3a5d2130574e67b0007d858321fe901
zyk <1019437875@qq.com>
2023-05-05 18:01:44 +0800

    asd

---
+++ src/main/scala/DemoFromKafka2Hive.scala
@@ -0,0 +1,23 @@
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+object DemoFromKafka2Hive {
+  // spark上下文
+  val spark = SparkSession.builder().appName("kafka_test").master("local[*]").enableHiveSupport().getOrCreate()
+  import spark.implicits._
+
+  def main(args: Array[String]): Unit = {
+      // 1. 接入kafka数据源
+      val df = spark.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers", "zyk-bigdata-002:9092")
+        .option("subscribe", "realtime_data")
+        .load()
+
+    // kafka读出来默认是二进制，转为string
+    val value: Dataset[String] = df.selectExpr("cast(value as STRING)").map(row => {
+      row
+    })
+
+
+  }
+}
--- src/main/scala/StructuredStreamingConsumer.scala
+++ src/main/scala/StructuredStreamingConsumer.scala
@@ -1,7 +1,11 @@
-import org.apache.spark.sql.streaming.{OutputMode, Trigger}
-import org.apache.spark.sql.{Dataset, Row, SaveMode, SparkSession}
+import com.alibaba.fastjson.JSON
+import org.apache.spark.sql.streaming.{OutputMode, ProcessingTime, Trigger}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SaveMode, SparkSession}
 
+import java.util
 import java.util.concurrent.TimeUnit
+import scala.concurrent.duration.DurationInt
+import scala.util.parsing.json.JSONObject
 
 object StructuredStreamingConsumer {
   // spark上下文
@@ -11,33 +15,40 @@ object StructuredStreamingConsumer {
   def main(args: Array[String]): Unit = {
     val df = spark.readStream
       .format("kafka")
-      .option("kafka.bootstrap.servers","43.139.144.33:9092")
+      .option("kafka.bootstrap.servers","zyk-bigdata-002:9092")
       .option("subscribe","quickstart-events")
       .load()
 
-    val value: Dataset[String] = df.selectExpr("cast(value as STRING)").map(row => {
-      row.getString(0)
-    })
+//    读kafka取出 message并做基本的清洗，默认值，添加上年月日字段
+    val value: DataFrame = df.selectExpr("cast(value as STRING)").map(row => {
+      val kafkaStr = row.getString(0)
+      val message = JSON.parseObject(kafkaStr).getString("message")
+      val msgArray = message.split(",") //指定分隔符进行字段切分
+      msgArray
+    }).filter(_.length == 9) //只留字段数为9的数据
+      .filter(array => array(2).length >= 8) //确保日期字段符合规范
+      .map(array => (array(0) + array(1) + array(2)
+        , array(0)
+        , array(1)
+        , array(2)
+        , array(3)
+        ,if (array(4).isInstanceOf[Int]) array(4).toInt else 99
+        , array(5)
+        , array(6)
+        , array(7)
+        , array(8)
+        , array(2).substring(0, 4)
+        , array(2).substring(4, 6)
+        , array(2).substring(6, 8)
+      ))
+      .toDF("id","client_ip","domain","time","target_ip","rcode","query_type","authority_record","add_msg","dns_ip","year","month","day")
 
-    val df1 = spark.table("src")
-    df1.write.mode(SaveMode.Overwrite).saveAsTable("hive_records")
-
-    value.printSchema()
     value.writeStream
       .outputMode(OutputMode.Append())
-      .format("orc")  //指定外部输出的文件存储格式
-      .option("format", "append")
-      .trigger(Trigger.ProcessingTime(10,TimeUnit.SECONDS))/**每60秒执行一次，不指定就是as fast as possible*/
-      .option("checkpointLocation","hdfs:/xxxxx:8020/tmp/offset/test/StructuredStreamingFromKafka2Hive") /**用来保存offset，用该目录来绑定对应的offset，如果该目录发生改变则程序运行的id会发生变化，类比group.id的变化，写hive的时候一定不要轻易变动*/
-      .partitionBy("year","month","day")//提供分区字段
-
-
-
-
-//    value.writeStream
-//      .outputMode(OutputMode.Append())
-//      .format("console")
-//      .start()
+      .format("parquet")
+      .partitionBy("year","month","day")
+      .trigger(Trigger.ProcessingTime(60,TimeUnit.SECONDS))  // 写入间隔
+      .
 
   }
 }

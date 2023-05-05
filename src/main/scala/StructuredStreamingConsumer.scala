3111f7df19dada2c9235885df4048d9606e4dfee
zyk <1019437875@qq.com>
2023-05-05 16:26:44 +0800

    asdf

--- src/main/scala/StructuredStreamingConsumer.scala
+++ src/main/scala/StructuredStreamingConsumer.scala
@@ -1,9 +1,11 @@
-import org.apache.spark.sql.streaming.OutputMode
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
+import org.apache.spark.sql.{Dataset, Row, SaveMode, SparkSession}
+
+import java.util.concurrent.TimeUnit
 
 object StructuredStreamingConsumer {
   // spark上下文
-  val spark = SparkSession.builder().appName("kafka_test").master("local[*]").getOrCreate()
+  val spark = SparkSession.builder().appName("kafka_test").master("local[*]").enableHiveSupport().getOrCreate()
   import spark.implicits._
 
   def main(args: Array[String]): Unit = {
@@ -17,11 +19,25 @@ object StructuredStreamingConsumer {
       row.getString(0)
     })
 
-    value.printSchema()
+    val df1 = spark.table("src")
+    df1.write.mode(SaveMode.Overwrite).saveAsTable("hive_records")
 
+    value.printSchema()
     value.writeStream
       .outputMode(OutputMode.Append())
-      .format("console")
-      .start()
+      .format("orc")  //指定外部输出的文件存储格式
+      .option("format", "append")
+      .trigger(Trigger.ProcessingTime(10,TimeUnit.SECONDS))/**每60秒执行一次，不指定就是as fast as possible*/
+      .option("checkpointLocation","hdfs:/xxxxx:8020/tmp/offset/test/StructuredStreamingFromKafka2Hive") /**用来保存offset，用该目录来绑定对应的offset，如果该目录发生改变则程序运行的id会发生变化，类比group.id的变化，写hive的时候一定不要轻易变动*/
+      .partitionBy("year","month","day")//提供分区字段
+
+
+
+
+//    value.writeStream
+//      .outputMode(OutputMode.Append())
+//      .format("console")
+//      .start()
+
   }
 }
